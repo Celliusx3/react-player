33dd597e33e1d2f302af6dcce8f404af8d930cee
Coyan Oh <cellius.yan@gmail.com>
2020-04-22 21:14:34 +0800

    Updates

---
+++ precache-manifest.207006e0544ec85b3465d645108009ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fcb1966705b307df1ea86d01b5eb4b2",
+    "url": "/react-player/index.html"
+  },
+  {
+    "revision": "371f7b1ce5d2b130572c",
+    "url": "/react-player/static/css/main.8df78410.chunk.css"
+  },
+  {
+    "revision": "7cdc79ff49ae9bfa15e2",
+    "url": "/react-player/static/js/2.6abedd35.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-player/static/js/2.6abedd35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "371f7b1ce5d2b130572c",
+    "url": "/react-player/static/js/main.9de24d26.chunk.js"
+  },
+  {
+    "revision": "6001193782312f3fe452",
+    "url": "/react-player/static/js/runtime-main.1d76a469.js"
+  }
+]);
